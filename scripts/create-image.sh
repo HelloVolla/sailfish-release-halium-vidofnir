a02c342784b93712f677c6bb4a4ad9795edde143
Adam Pigg <adam@piggz.co.uk>
2025-03-08 21:02:45 +0000

    Clean up super creation sizes

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -101,7 +101,17 @@ export LDFLAGS="-lstdc++fs -L/usr/lib/gcc/aarch64-meego-linux-gnuabi/8.3.0/"
 #tar xvJf ubuntu-touch-mimameid-firmware-r.tar.xz
 
 echo "Create super.img..."
-./lpunpack_and_lpmake/bin/lpmake --metadata-size 65536 --metadata-slots 1 --sparse --super-name super --device super:8589934592 --group sailfish:8585740288 --partition system_a:none:8388608000:sailfish --image system_a=$OUTPUTDIR/sfe-$DEVICE-$RELEASE$EXTRA_NAME/root.img --output $OUTPUTDIR/sfe-$DEVICE-$RELEASE$EXTRA_NAME/super.img
+kib=$((2**10)); mib=$((2**20))
+super_max_size=$((8192*$mib))
+group_size=$((super_max_size-$((4*$mib))))
+root_size=$((group_size-$((4*$mib))))
+./lpunpack_and_lpmake/bin/lpmake \
+	--metadata-size $((64*$kib)) --metadata-slots 1 \
+	--sparse --super-name super \
+	--device super:$super_max_size --group sailfish:$group_size \
+	--partition system_a:none:$root_size:sailfish \
+	--image system_a=$OUTPUTDIR/sfe-$DEVICE-$RELEASE$EXTRA_NAME/root.img \
+	--output $OUTPUTDIR/sfe-$DEVICE-$RELEASE$EXTRA_NAME/super.img
 
 echo "Pack final image..."
 mv $OUTPUTDIR/sfe-$DEVICE-$RELEASE$EXTRA_NAME $OUTPUTDIR/SailfishOS-$DEVICE
